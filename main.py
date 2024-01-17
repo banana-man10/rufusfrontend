861e49c946c6cba5871a01b087b6300350bf1528
silverstero <67803641+banana-man10@users.noreply.github.com>
2024-01-17 16:09:58 +0000

    Add SUPABASE_URL and SUPABASE_KEY to .env file, update main.py to use Supabase client, and modify index.html to include an embed tag

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import flask
+import os
+from supabase import create_client, Client
+
+url: str = "https://gzmpxnzxklmvrnbchcra.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6bXB4bnp4a2xtdnJuYmNoY3JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ5MTE3OTcsImV4cCI6MjAyMDQ4Nzc5N30.oFrBquw8pe1wWEHS9uggSykkuW8-LsNdSt7bJvrBvXk"
+supabase: Client = create_client(url, key)
 
 app = flask.Flask(__name__)
 
+
+
 @app.route('/')
 def index():
-    name = 'John'
-    age = '73'
-    return flask.render_template('index.html', name=name, age=age)
+    return flask.render_template('index.html')
 
-
-@app.route('/witty')
-def create():
-    return flask.render_template('makeawitty.html')
-app.run(port=5000)
